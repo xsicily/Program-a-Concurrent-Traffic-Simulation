c076b082554276721e5ba07d95fc7581ebfbcce9
sicily <sicily.wang@outlook.com>
2020-05-04 22:43:15 +0300

    modification after Udacity review

--- src/Intersection.cpp
+++ src/Intersection.cpp
@@ -86,14 +86,16 @@ void Intersection::addVehicleToQueue(std::shared_ptr<Vehicle> vehicle)
     // wait until the vehicle is allowed to enter
     ftrVehicleAllowedToEnter.wait();
     lck.lock();
-    std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
-    
+
     // FP.6b : use the methods TrafficLight::getCurrentPhase and TrafficLight::waitForGreen 
     // to block the execution until the traffic light turns green.
+    
     if (!trafficLightIsGreen()){
         _trafficLight.waitForGreen();
     }
 
+    std::cout << "Intersection #" << _id << ": Vehicle #" << vehicle->getID() << " is granted entry." << std::endl;
+    
     lck.unlock();
 }
 
